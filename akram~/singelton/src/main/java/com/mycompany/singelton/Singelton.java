830cbb63f543de8373791c1a221fb32e722e2c07
AKRAMDABOR <132850579+AKRAMDABOR@users.noreply.github.com>
2023-05-31 01:04:36 +0300

    Add files via upload

---
+++ akram~/singelton/src/main/java/com/mycompany/singelton/Singelton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.singelton;
+
+/**
+ *
+ * @author khaled
+ */
+public class Singelton {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
